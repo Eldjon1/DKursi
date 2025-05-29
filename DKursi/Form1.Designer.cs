f4e913799d0fe1564de7a51e90e96bacb9a3701d
Eldjon1 <eldoshaqiri7@gmail.com>
2025-05-29 20:29:04 +0200

    Forma

--- DKursi/Form1.Designer.cs
+++ DKursi/Form1.Designer.cs
@@ -1,16 +1,9 @@
 ﻿namespace DKursi
 {
-    partial class Form1
+    partial class LoginForm
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +15,121 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            lblPerdoruesi = new Label();
+            txtPerdoruesi = new TextBox();
+            txtFjalekalimi = new TextBox();
+            lblFjalekalimi = new Label();
+            btnHyr = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.CadetBlue;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(208, 349);
+            panel1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(46, 76);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(127, 111);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // lblPerdoruesi
+            // 
+            lblPerdoruesi.AutoSize = true;
+            lblPerdoruesi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPerdoruesi.Location = new Point(244, 35);
+            lblPerdoruesi.Name = "lblPerdoruesi";
+            lblPerdoruesi.Size = new Size(131, 32);
+            lblPerdoruesi.TabIndex = 3;
+            lblPerdoruesi.Text = "Perdoruesi:";
+            lblPerdoruesi.Click += lblPerdoruesi_Click;
+            // 
+            // txtPerdoruesi
+            // 
+            txtPerdoruesi.Location = new Point(244, 76);
+            txtPerdoruesi.Name = "txtPerdoruesi";
+            txtPerdoruesi.Size = new Size(364, 31);
+            txtPerdoruesi.TabIndex = 0;
+            // 
+            // txtFjalekalimi
+            // 
+            txtFjalekalimi.Location = new Point(244, 176);
+            txtFjalekalimi.Name = "txtFjalekalimi";
+            txtFjalekalimi.Size = new Size(364, 31);
+            txtFjalekalimi.TabIndex = 1;
+            txtFjalekalimi.UseSystemPasswordChar = true;
+            txtFjalekalimi.TextChanged += txtFjalekalimi_TextChanged;
+            // 
+            // lblFjalekalimi
+            // 
+            lblFjalekalimi.AutoSize = true;
+            lblFjalekalimi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblFjalekalimi.Location = new Point(244, 135);
+            lblFjalekalimi.Name = "lblFjalekalimi";
+            lblFjalekalimi.Size = new Size(131, 32);
+            lblFjalekalimi.TabIndex = 4;
+            lblFjalekalimi.Text = "Fjalekalimi:";
+            lblFjalekalimi.Click += lblFjalekalimi_Click;
+            // 
+            // btnHyr
+            // 
+            btnHyr.BackgroundImage = (Image)resources.GetObject("btnHyr.BackgroundImage");
+            btnHyr.BackgroundImageLayout = ImageLayout.Stretch;
+            btnHyr.Location = new Point(375, 230);
+            btnHyr.Name = "btnHyr";
+            btnHyr.Size = new Size(92, 73);
+            btnHyr.TabIndex = 2;
+            btnHyr.UseVisualStyleBackColor = true;
+            btnHyr.Click += btnHyr_Click;
+            // 
+            // LoginForm
+            // 
+            AcceptButton = btnHyr;
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PapayaWhip;
+            ClientSize = new Size(656, 349);
+            Controls.Add(btnHyr);
+            Controls.Add(txtFjalekalimi);
+            Controls.Add(lblFjalekalimi);
+            Controls.Add(txtPerdoruesi);
+            Controls.Add(lblPerdoruesi);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "LoginForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            Load += LoginForm_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label lblPerdoruesi;
+        private TextBox txtPerdoruesi;
+        private TextBox txtFjalekalimi;
+        private Label lblFjalekalimi;
+        private Button btnHyr;
     }
 }
--- DKursi/Form1.cs
+++ DKursi/Form1.cs
@@ -1,10 +1,35 @@
 namespace DKursi
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPerdoruesi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFjalekalimi_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblFjalekalimi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHyr_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
