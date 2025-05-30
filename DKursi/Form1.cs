897f34f2755d7fb6dcc00d3dcc6d4f393fd0ca4b
ergi-bregu <ergi.bregu111@gmail.com>
2025-05-30 13:28:52 +0200

    Shtimi i log in, dhe i databases se user test

--- DKursi/Form1.cs
+++ DKursi/Form1.cs
@@ -26,9 +26,35 @@ namespace DKursi
         {
 
         }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            Application.Exit();
+        }
+
 
         private void btnHyr_Click(object sender, EventArgs e)
         {
+        {
+            string username = txtPerdoruesi.Text;
+            string password = txtFjalekalimi.Text;
+
+            if (users.ContainsKey(username) && users[username] == password)
+            {
+                MessageBox.Show("Hyrje e suksesshme!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                
+
+                this.Hide(); // Fshih formën e loginit
+            }
+            else
+            {
+                MessageBox.Show("Emri i përdoruesit ose fjalëkalimi është i pasaktë.", "Gabim", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
 
         }
     }
--- DKursi/Program.cs
+++ DKursi/Program.cs
@@ -1,3 +1,4 @@
+
 namespace DKursi
 {
     internal static class Program
